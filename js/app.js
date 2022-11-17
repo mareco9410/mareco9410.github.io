db5f974f5f363d9678020a8ab8819ccceb9e645b
Diego Mareco <mareco9410@gmail.com>
2022-11-17 15:54:36 -0300

    Ultimo commit 2

--- js/app.js
+++ js/app.js
@@ -1,18 +1,17 @@
 /*
 Validamos la URL para ver si estoy en DEV (localhost) o en PROD (github.pages)
  */
-var url = window.location.href;
-var swLocation = '/sw.js';
+//var url = window.location.href;
+//var swLocation = '/sw.js';
 
 //Registro el SW
 if (navigator.serviceWorker) {
-    console.log('ENTRO AL IF')
-    console.log(url);
-    if ( url.includes('localhost')) {
-        console.log('ENTRO AL IF 2')
+
+ //   if ( url.includes('localhost')) {
+
         swLocation = '/sw.js';
-    }
-    console.log('Valor de swLocaion: ' + swLocation);
+ //   }
+
     navigator.serviceWorker.register( swLocation );
 }
 
